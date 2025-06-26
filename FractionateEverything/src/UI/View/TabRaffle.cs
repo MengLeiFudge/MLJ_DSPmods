39d2c0f0de62414b9fe49de6acaae9814a8f95d0
萌泪酱 <605738729@qq.com>
2025-06-26 19:30:15 +0700

    绘制新图标

--- FractionateEverything/src/UI/View/TabRaffle.cs
+++ FractionateEverything/src/UI/View/TabRaffle.cs
@@ -21,9 +21,15 @@ public static class TabRaffle {
     /// <summary>
     /// 下一抽是第几抽。
     /// </summary>
-    public static int RaffleCount = 1;
+    public static int RecipeRaffleCount = 1;
+    public static double RecipeRaffleRate => 0.006 + Math.Max(RecipeRaffleCount - 73, 0) * 0.06;
 
-    public static double RecipeRate => 0.006 + Math.Max(RaffleCount - 73, 0) * 0.06;
+    /// <summary>
+    /// 下一抽是第几抽。
+    /// </summary>
+    public static int BuildingRaffleCount = 1;
+    public static double BuildingRaffleRate => 0.006 + Math.Max(BuildingRaffleCount - 73, 0) * 0.06;
+
     public static double[] FracProtoArr = [0.05, 0.02, 0.01, 0.005, 0.002, 0.001];
     public static int[] FracProtoID = [IFE分馏原胚普通, IFE分馏原胚精良, IFE分馏原胚稀有, IFE分馏原胚史诗, IFE分馏原胚传说, IFE分馏原胚定向];
     public static void LoadConfig(ConfigFile configFile) { }
@@ -34,7 +40,7 @@ public static class TabRaffle {
         float y;
         wnd.AddTabGroup(trans, "抽卡", "tab-group-fe2");
         {
-            var tab = wnd.AddTab(trans, "抽卡");
+            var tab = wnd.AddTab(trans, "配方卡池");
             x = 0f;
             y = 10f;
             wnd.AddComboBox(x, y, tab, "卡池选择").WithItems(RecipeTypeNames).WithSize(150f, 0f)
@@ -44,6 +50,18 @@ public static class TabRaffle {
                 () => Raffle(RecipeTypes[RecipeTypeEntry.Value], 1));
             wnd.AddButton(x + 300, y, 200, tab, "十连", 16, "button-raffle-10",
                 () => Raffle(RecipeTypes[RecipeTypeEntry.Value], 10));
+        }
+        {
+            var tab = wnd.AddTab(trans, "建筑卡池");
+            x = 0f;
+            y = 10f;
+            wnd.AddComboBox(x, y, tab, "卡池选择").WithItems(BuildingTypeNames).WithSize(150f, 0f)
+                .WithConfigEntry(BuildingTypeEntry);
+            y += 38f;
+            wnd.AddButton(x, y, 200, tab, "单抽", 16, "button-raffle-1",
+                () => Raffle(BuildingTypeEntry.Value, 1));
+            wnd.AddButton(x + 300, y, 200, tab, "十连", 16, "button-raffle-10",
+                () => Raffle(BuildingTypeEntry.Value, 10));
         }
     }
 
@@ -62,24 +80,70 @@ public static class TabRaffle {
         StringBuilder sb = new StringBuilder("获得了以下物品：");
         while (count > 0) {
             count--;
-            double currRate = RecipeRate;
+            double currRate = RecipeRaffleRate;
             double randDouble = random.NextDouble();
             //配方
             if (randDouble < currRate) {
                 int idx = random.Next(0, recipeArr.Count);
                 BaseRecipe recipe = recipeArr[idx];
+                string name = LDB.items.Select(recipe.InputID).name.Trim('\r', '\n');
                 if (!recipe.IsUnlocked) {
                     recipe.Level = 1;
                     recipe.Quality = 1;
-                    sb.AppendLine($"\n[配方] {recipeType.GetName()}-{LDB.items.Select(recipe.InputID).name.Trim()}");
+                    sb.AppendLine($"\n[配方] {recipeType.GetName()}-{name}");
                 } else {
                     recipe.MemoryCount++;
-                    sb.AppendLine($"\n[回响] {recipeType.GetName()}-{LDB.items.Select(recipe.InputID).name.Trim()}");
+                    sb.AppendLine($"\n[回响] {recipeType.GetName()}-{name}");
                 }
-                RaffleCount = 1;
+                RecipeRaffleCount = 1;
                 continue;
             }
-            RaffleCount++;
+            RecipeRaffleCount++;
+            //分馏原胚
+            bool getFracProto = false;
+            for (int i = 0; i < FracProtoArr.Length; i++) {
+                currRate += FracProtoArr[i];
+                if (randDouble < currRate) {
+                    GameMain.mainPlayer.TryAddItemToPackage(FracProtoID[i], 1, 0, true);
+                    sb.Append($"\n[原胚] {LDB.items.Select(FracProtoID[i]).name} x 1");
+                    getFracProto = true;
+                    break;
+                }
+            }
+            if (getFracProto) {
+                continue;
+            }
+            //狗粮
+            GameMain.mainPlayer.TryAddItemToPackage(I沙土, 1000, 0, true);
+            sb.Append("\n[沙土] 沙土 x 1000");
+        }
+        UIMessageBox.Show("抽卡结果", sb.ToString(), "确认", UIMessageBox.INFO);
+    }
+
+    /// <summary>
+    /// 抽卡。
+    /// </summary>
+    /// <param name="buildingType">建筑奖池</param>
+    /// <param name="count">抽卡次数</param>
+    public static void Raffle(int buildingType, int count) {
+        if (DSPGame.IsMenuDemo || GameMain.mainPlayer == null) {
+            return;
+        }
+        StringBuilder sb = new StringBuilder("获得了以下物品：");
+        while (count > 0) {
+            count--;
+            double currRate = BuildingRaffleRate;
+            double randDouble = random.NextDouble();
+            //建筑
+            if (randDouble < currRate) {
+                ItemProto item = LDB.items.Select(BuildingIds[buildingType]);
+                string name = item.name.Trim('\r', '\n');
+                GameMain.mainPlayer.TryAddItemToPackage(item.ID, 1, 0, true);
+                sb.AppendLine($"\n[建筑] {name} x 1");
+                BuildingRaffleCount = 1;
+                continue;
+            }
+            BuildingRaffleCount++;
             //分馏原胚
             bool getFracProto = false;
             for (int i = 0; i < FracProtoArr.Length; i++) {
@@ -105,16 +169,16 @@ public static class TabRaffle {
 
     public static void Import(BinaryReader r) {
         int version = r.ReadInt32();
-        RaffleCount = r.ReadInt32();
+        RecipeRaffleCount = r.ReadInt32();
     }
 
     public static void Export(BinaryWriter w) {
         w.Write(1);
-        w.Write(RaffleCount);
+        w.Write(RecipeRaffleCount);
     }
 
     public static void IntoOtherSave() {
-        RaffleCount = 0;
+        RecipeRaffleCount = 0;
     }
 
     #endregion
