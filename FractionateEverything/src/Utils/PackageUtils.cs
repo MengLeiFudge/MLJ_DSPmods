8686d7260681570c91aef0244ea44d3a5b91fa6d
萌泪酱 <605738729@qq.com>
2025-08-01 22:16:33 +0700

    统计面板显示精华消耗，沙土计入分馏塔产物计数

--- FractionateEverything/src/Utils/PackageUtils.cs
+++ FractionateEverything/src/Utils/PackageUtils.cs
@@ -262,7 +262,7 @@ public static partial class Utils {
     /// 如果数目不足，则不拿取；否则扣除对应物品。
     /// 注意，为了提高性能，此方法未判断某些前置条件。使用时需注意情况。
     /// </summary>
-    public static bool TakeEssenceFromModData(int n) {
+    public static bool TakeEssenceFromModData(int n, int[] consumeRegister) {
         lock (itemModDataCount) {
             if (GetModDataItemCount(IFE复制精华) < n
                 || GetModDataItemCount(IFE点金精华) < n
@@ -274,6 +274,12 @@ public static partial class Utils {
             TakeItemFromModData(IFE点金精华, n);
             TakeItemFromModData(IFE分解精华, n);
             TakeItemFromModData(IFE转化精华, n);
+            lock (consumeRegister) {
+                consumeRegister[IFE复制精华] += n;
+                consumeRegister[IFE点金精华] += n;
+                consumeRegister[IFE分解精华] += n;
+                consumeRegister[IFE转化精华] += n;
+            }
             return true;
         }
     }
