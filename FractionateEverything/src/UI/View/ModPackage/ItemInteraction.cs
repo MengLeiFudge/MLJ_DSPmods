14bcec77d0f6207a9b22a0f35223f05adb86f2e2
萌泪酱 <605738729@qq.com>
2025-08-17 23:10:15 +0700

    添加物品交互界面的翻译

--- FractionateEverything/src/UI/View/ModPackage/ItemInteraction.cs
+++ FractionateEverything/src/UI/View/ModPackage/ItemInteraction.cs
@@ -12,9 +12,8 @@ public static class ItemInteraction {
     private static RectTransform window;
     private static RectTransform tab;
 
-    private static ItemProto SelectedItem { get; set; } = LDB.items.Select(I铁矿);
-    private static int SelectedItemId => SelectedItem.ID;
     private static Text textCurrItem;
+    private static ItemProto SelectedItem { get; set; } = LDB.items.Select(I铁矿);
     private static MyImageButton btnSelectedItem;
 
     private static void OnButtonChangeItemClick() {
@@ -34,6 +33,22 @@ public static class ItemInteraction {
 
     public static void AddTranslations() {
         Register("物品交互", "Item Interaction");
+
+        Register("物品交互提示按钮说明1",
+            "Left-click to select the items you want to query or extract.",
+            "左键选择需要查询或提取的物品。");
+
+        Register("当前共有", "There are currently");
+        Register("，其中分馏数据中心", "items, including Fractionation data center");
+        Register("，物流清单", ", logistics list");
+        Register("，个人背包", ", personal package");
+
+        Register("提取1组（", "Extract 1 group (");
+        Register("提取10组（", "Extract 10 group (");
+        Register("提取全部（", "Extract all (");
+        Register("分馏数据中心没有", "Fractionation data center have no");
+        Register("已从分馏数据中心提取：", "Extracted from fractionation data center: ");
+        Register("！", "!");
     }
 
     public static void LoadConfig(ConfigFile configFile) { }
@@ -43,22 +58,22 @@ public static class ItemInteraction {
         tab = wnd.AddTab(trans, "物品交互");
         float x = 0f;
         float y = 20f;
-        textCurrItem = wnd.AddText2(x, y, tab, "当前物品：", 15, "textCurrItem");
+        textCurrItem = wnd.AddText2(x, y, tab, "当前物品", 15, "textCurrItem");
         btnSelectedItem = wnd.AddImageButton(x + textCurrItem.preferredWidth + 5f, y, tab,
             SelectedItem.ID, "button-change-item",
             OnButtonChangeItemClick, OnButtonChangeItemClick,
-            "切换说明", "左键选择需要查询或提取的物品");
+            "提示", "物品交互提示按钮说明1");
         //todo: 修复按钮提示窗后移除该内容
         wnd.AddTipsButton2(x + textCurrItem.preferredWidth + 5f + 60, y, tab,
-            "切换说明", "左键选择需要查询或提取的物品");
+            "提示", "物品交互提示按钮说明1");
         y += 50f;
-        textItemCountInfo = wnd.AddText2(x, y, tab, "mod：xx 物流：xx 背包：xx", 15, "textItemCountInfo");
+        textItemCountInfo = wnd.AddText2(x, y, tab, "动态刷新", 15, "textItemCountInfo");
         y += 36f;
-        btnGetModDataItem[0] = wnd.AddButton(0, 3, y, tab, "提取1组物品", 16, "button-get-item0",
+        btnGetModDataItem[0] = wnd.AddButton(0, 3, y, tab, "动态刷新", 16, "button-get-item0",
             () => GetModDataItem(1));
-        btnGetModDataItem[1] = wnd.AddButton(1, 3, y, tab, "提取10组物品", 16, "button-get-item1",
+        btnGetModDataItem[1] = wnd.AddButton(1, 3, y, tab, "动态刷新", 16, "button-get-item1",
             () => GetModDataItem(10));
-        btnGetModDataItem[2] = wnd.AddButton(2, 3, y, tab, "提取全部物品", 16, "button-get-item2",
+        btnGetModDataItem[2] = wnd.AddButton(2, 3, y, tab, "动态刷新", 16, "button-get-item2",
             () => GetModDataItem(-1));
         y += 36f;
     }
@@ -68,26 +83,26 @@ public static class ItemInteraction {
             return;
         }
         btnSelectedItem.SetSprite(SelectedItem.iconSprite);
-        textItemCountInfo.text = $"当前共有 {GetItemTotalCount(SelectedItem.ID)}，其中"
-                                 + $"数据中心 {GetModDataItemCount(SelectedItem.ID)}，"
-                                 + $"物流背包 {GetDeliveryPackageItemCount(SelectedItem.ID)}，"
-                                 + $"个人背包 {GetPackageItemCount(SelectedItem.ID)}";
+        textItemCountInfo.text = $"{"当前共有".Translate()} {GetItemTotalCount(SelectedItem.ID)}"
+                                 + $"{"，其中分馏数据中心".Translate()} {GetModDataItemCount(SelectedItem.ID)}"
+                                 + $"{"，物流清单".Translate()} {GetDeliveryPackageItemCount(SelectedItem.ID)}"
+                                 + $"{"，个人背包".Translate()} {GetPackageItemCount(SelectedItem.ID)}";
         var t = btnGetModDataItem[0].gameObject.transform.Find("button-text").GetComponent<Text>();
         if (t != null) {
-            t.text = $"提取1组（{SelectedItem.StackSize}）";
+            t.text = $"{"提取1组（".Translate()}{SelectedItem.StackSize}{"）".Translate()}";
         }
         //enabled -> 启用/禁用    gameObject.SetActive -> 显示/隐藏
         btnGetModDataItem[0].enabled = GetModDataItemCount(SelectedItem.ID) >= SelectedItem.StackSize;
         btnGetModDataItem[0].button.enabled = GetModDataItemCount(SelectedItem.ID) >= SelectedItem.StackSize;
         t = btnGetModDataItem[1].gameObject.transform.Find("button-text").GetComponent<Text>();
         if (t != null) {
-            t.text = $"提取10组（{SelectedItem.StackSize * 10}）";
+            t.text = $"{"提取10组（".Translate()}{SelectedItem.StackSize * 10}{"）".Translate()}";
         }
         btnGetModDataItem[1].enabled = GetModDataItemCount(SelectedItem.ID) >= SelectedItem.StackSize * 10;
         btnGetModDataItem[1].button.enabled = GetModDataItemCount(SelectedItem.ID) >= SelectedItem.StackSize * 10;
         t = btnGetModDataItem[2].gameObject.transform.Find("button-text").GetComponent<Text>();
         if (t != null) {
-            t.text = $"提取全部（{GetModDataItemCount(SelectedItem.ID)}）";
+            t.text = $"{"提取全部（".Translate()}{GetModDataItemCount(SelectedItem.ID)}{"）".Translate()}";
         }
         btnGetModDataItem[2].enabled = GetModDataItemCount(SelectedItem.ID) > 0;
         btnGetModDataItem[2].button.enabled = GetModDataItemCount(SelectedItem.ID) > 0;
@@ -101,10 +116,16 @@ public static class ItemInteraction {
         int count = groupCount == -1 ? int.MaxValue : SelectedItem.StackSize * groupCount;
         count = TakeItemFromModData(SelectedItem.ID, count, out int inc);
         if (count == 0) {
-            UIMessageBox.Show("提示".Translate(), $"分馏数据中心没有物品 {SelectedItem.name} ！", "确定".Translate(), UIMessageBox.WARNING);
+            UIMessageBox.Show("提示".Translate(),
+                $"{"分馏数据中心没有".Translate()} {SelectedItem.name} {"！".Translate()}",
+                "确定".Translate(),
+                UIMessageBox.WARNING);
         } else {
             AddItemToPackage(SelectedItem.ID, count, inc, false);
-            UIMessageBox.Show("提示".Translate(), $"已从分馏数据中心提取 {SelectedItem.name} x {count} ！", "确定".Translate(), UIMessageBox.INFO);
+            UIMessageBox.Show("提示".Translate(),
+                $"{"已从分馏数据中心提取：".Translate()}{SelectedItem.name} x {count} {"！".Translate()}",
+                "确定".Translate(),
+                UIMessageBox.INFO);
         }
     }
 
