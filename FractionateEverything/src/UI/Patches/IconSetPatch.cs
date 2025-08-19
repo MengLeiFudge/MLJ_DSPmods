1e49aa619e12a898d8d56a4d2c5fe71bbe9c4e15
萌泪酱 <605738729@qq.com>
2025-08-20 01:56:38 +0700

    优化限时商店页面

--- FractionateEverything/src/UI/Patches/IconSetPatch.cs
+++ FractionateEverything/src/UI/Patches/IconSetPatch.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using HarmonyLib;
 using UnityEngine;
-using static FE.Utils.Utils;
 using Object = UnityEngine.Object;
 
 namespace FE.UI.Patches;
