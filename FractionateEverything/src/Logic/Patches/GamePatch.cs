d9eafc73a506384923e34cf8df18d5c96c0c410a
乡下来的喵 <38677850+xxldm@users.noreply.github.com>
2025-09-05 21:56:14 +0800

    添加交互物流塔

---
+++ FractionateEverything/src/Logic/Patches/GamePatch.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Linq;
+using HarmonyLib;
+
+using static FE.Utils.Utils;
+
+namespace FE.Logic.Patches;
+
+public static class GamePatch {
+    private static long _lastTickTime;
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(PlanetTransport), "GameTick")]
+    public static void PlanetTransportGameTickPostPatch(long time, PlanetTransport __instance) {
+        try {
+            // 设置一个运行间隔
+            if (time % 60L != 0L || time == _lastTickTime) {
+                return;
+            }
+            // 记住上一次运行时间，避免连续运行2次
+            _lastTickTime = time;
+            // 打乱所有物流塔的顺序，尽可能的让每个物流塔都有机会拿到物品
+            // TODO 要不加个开关，毕竟需要消耗性能
+            var tmpStationPool = ConfusionArray(__instance.stationPool);
+            foreach (var sc in tmpStationPool) {
+                // 筛选出所有的交互物流塔
+                if (sc == null || sc.isCollector || sc.isVeinCollector ||
+                    __instance.factory.entityPool[sc.entityId].protoId != 8028) {
+                    continue;
+                }
+                // 循环所有的栏位
+                for (var i = 0; i < sc.storage.Length; ++i) {
+                    ref var store = ref sc.storage[i];
+                    // 暂存栏位数据
+                    var storeCount = store.count;
+                    var storeInc = store.inc;
+                    // TODO 要不根据物品堆叠，消耗电量？
+                    // 1堆叠10%的消耗
+                    switch (store.localLogic) {
+                        case ELogisticStorage.Supply: {
+                            // 供应 = 从数据中心下载到塔里，然后提供出去
+                            // 计算需求的数量
+                            var count = store.max - storeCount;
+                            // 下载物品
+                            TakeItem(ref store, count);
+                            break;
+                        }
+                        case ELogisticStorage.Demand: {
+                            // 需求 = 需求物品到塔里，然后上传数据中心
+                            // 如果存在物品，则上传
+                            if (storeCount > 0) {
+                                // 上传物品
+                                AddItem(ref store, storeCount, storeInc);
+                            }
+                            break;
+                        }
+                        case ELogisticStorage.None: {
+                            // 仓储 = 维持数目为上限的一半
+                            // 计算上限一半的数量
+                            var num = store.max / 2;
+                            if (storeCount < num) {
+                                // 如果数量小于一半
+                                // 计算下载的数量
+                                var count = num - storeCount;
+                                // 下载物品
+                                TakeItem(ref store, count);
+                            } else if (storeCount > num) {
+                                // 如果数量大于一半
+                                // 计算上传的数量
+                                var count = storeCount - num;
+                                // 计算上传的增产点
+                                var inc = count / storeCount * storeInc;
+                                // 上传物品
+                                AddItem(ref store, count, inc);
+                            }
+                            break;
+                        }
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                }
+            }
+        } catch (Exception ex) {
+            LogError(ex);
+        }
+    }
+
+    private static void AddItem(ref StationStore store, int count, int inc) {
+        // 上传物品到数据中心
+        AddItemToModData(store.itemId, count, inc);
+        // 移除交互物流塔中对应的数量
+        store.count -= count;
+        // 移除交互物流塔中对应的增产点
+        store.inc -= inc;
+    }
+
+    private static void TakeItem(ref StationStore store, int count) {
+        // 如果请求下载的数量小于0，则返回
+        if (count <= 0) {
+            return;
+        }
+        // 从数据中心获取物品数量
+        var modDataItemCount = GetModDataItemCount(store.itemId);
+        // 如果数据中心没有物品，则返回
+        if (modDataItemCount <= 0L) {
+            return;
+        }
+        // 从数据中心下载的增产点
+        // 从数据中心下载物品
+        var itemFromModData = TakeItemFromModData(store.itemId, count, out var inc);
+        // 添加物品到交互物流塔中
+        store.count += itemFromModData;
+        // 添加增产点到交互物流塔中
+        store.inc += inc;
+    }
+
+    private static StationComponent[] ConfusionArray(StationComponent[] arr) {
+        var random = new Random();
+        return arr
+            .OrderBy(_ => random.Next())
+            .ToArray();
+    }
+}
