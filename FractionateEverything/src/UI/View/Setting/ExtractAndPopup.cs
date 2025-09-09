def07c147521c6665e08ea73610cb2e633231ad3
萌泪酱 <605738729@qq.com>
2025-09-10 00:00:03 +0700

    可自定义物品消耗的顺序
    (cherry picked from commit c98e093e8aa862aff294081acdceda77d3a61f15)

--- FractionateEverything/src/UI/View/Setting/ExtractAndPopup.cs
+++ FractionateEverything/src/UI/View/Setting/ExtractAndPopup.cs
@@ -18,11 +18,34 @@ public static class ExtractAndPopup {
     private static ConfigEntry<int> RightClickTakeCountEntry;
     public static int RightClickTakeCount => ClickTakeCounts[RightClickTakeCountEntry.Value];
 
+    private static readonly string[] TakeItemPriorityStrs = [
+        $"{"背包".Translate()} -> {"物流背包".Translate()} -> {"分馏数据中心".Translate()}",
+        $"{"背包".Translate()} -> {"分馏数据中心".Translate()} -> {"物流背包".Translate()}",
+        $"{"物流背包".Translate()} -> {"背包".Translate()} -> {"分馏数据中心".Translate()}",
+        $"{"物流背包".Translate()} -> {"分馏数据中心".Translate()} -> {"背包".Translate()}",
+        $"{"分馏数据中心".Translate()} -> {"背包".Translate()} -> {"物流背包".Translate()}",
+        $"{"分馏数据中心".Translate()} -> {"物流背包".Translate()} -> {"背包".Translate()}",
+    ];
+    private static readonly int[][] TakeItemPriorityArr = [
+        [0, 1, 2],
+        [0, 2, 1],
+        [1, 0, 2],
+        [1, 2, 0],
+        [2, 0, 1],
+        [2, 1, 0],
+    ];
+    private static ConfigEntry<int> TakeItemPriorityEntry;
+    public static int[] TakeItemPriority => TakeItemPriorityArr[TakeItemPriorityEntry.Value];
+
     public static void AddTranslations() {
         Register("提取&弹窗", "Extract & Pop-up");
 
         Register("左键单击时提取几组物品", "Extract how many sets of items when left-click");
         Register("右键单击时提取几组物品", "Extract how many sets of items when right-click");
+
+        Register("背包", "Package");
+        Register("物流背包", "Delivery Package");
+        //Register("分馏数据中心", "Fractionation Data Centre");
     }
 
     public static void LoadConfig(ConfigFile configFile) {
@@ -33,6 +56,10 @@ public static class ExtractAndPopup {
         RightClickTakeCountEntry = configFile.Bind("Extract & Pop-up", "RightClickTakeCount", 3, "右键单击时提取几组物品");
         if (RightClickTakeCountEntry.Value < 0 || RightClickTakeCountEntry.Value >= ClickTakeCounts.Length) {
             RightClickTakeCountEntry.Value = 3;
+        }
+        TakeItemPriorityEntry = configFile.Bind("Extract & Pop-up", "TakeItemPriority", 1, "使用物品顺序");
+        if (TakeItemPriorityEntry.Value < 0 || TakeItemPriorityEntry.Value >= TakeItemPriorityArr.Length) {
+            TakeItemPriorityEntry.Value = 1;
         }
     }
 
@@ -46,6 +73,9 @@ public static class ExtractAndPopup {
         y += 36f;
         wnd.AddComboBox(x, y, tab, "右键单击时提取几组物品")
             .WithItems(ClickTakeCountsStr).WithSize(200, 0).WithConfigEntry(RightClickTakeCountEntry);
+        y += 36f;
+        wnd.AddComboBox(x, y, tab, "使用物品顺序")
+            .WithItems(TakeItemPriorityStrs).WithSize(400, 0).WithConfigEntry(TakeItemPriorityEntry);
     }
 
     public static void UpdateUI() {
