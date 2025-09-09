ec78e6b3f5807b3f3afdcae677441946c19f8204
萌泪酱 <605738729@qq.com>
2025-09-09 22:47:37 +0700

    修复op mod建筑师模式不生效的问题

--- FractionateEverything/src/Compatibility/SmelterMiner.cs
+++ FractionateEverything/src/Compatibility/SmelterMiner.cs
@@ -1,17 +1,22 @@
-﻿using BepInEx.Bootstrap;
+﻿using System.Reflection;
+using BepInEx.Bootstrap;
 using HarmonyLib;
 
 namespace FE.Compatibility;
 
 public static class SmelterMiner {
-    internal const string GUID = "Gnimaerd.DSP.plugin.SmelterMiner";
+    public const string GUID = "Gnimaerd.DSP.plugin.SmelterMiner";
+    public static bool Enable;
+    public static Assembly assembly;
 
-    internal static bool Enable;
-
-    internal static void Compatible() {
-        Enable = Chainloader.PluginInfos.TryGetValue(GUID, out _);
-        if (!Enable) return;
-        
+    public static void Compatible() {
+        Enable = Chainloader.PluginInfos.TryGetValue(GUID, out BepInEx.PluginInfo pluginInfo);
+        if (!Enable || pluginInfo == null) {
+            return;
+        }
+        assembly = pluginInfo.Instance.GetType().Assembly;
+        var harmony = new Harmony(PluginInfo.PLUGIN_GUID + ".Compatibility.SmelterMiner");
+        harmony.PatchAll(typeof(SmelterMiner));
         CheckPlugins.LogInfo("SmelterMiner Compat finish.");
     }
 }
