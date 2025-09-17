862a3b05f4b6ca55c87061cb30d637739954c4cb
萌泪酱 <605738729@qq.com>
2025-09-17 22:43:39 +0700

    优化GetDSPData项目结构

---
+++ GetDspData/src/Utils/FormatUtils.cs
@@ -0,0 +1,28 @@
+﻿namespace GetDspData.Utils;
+
+public static partial class Utils {
+    public static string Name(this Proto proto) {
+        return string.IsNullOrEmpty(proto.Name) ? proto.name : proto.Name;
+    }
+
+    public static string FName(this Proto proto) {
+        return proto.Name().Translate()
+            .Replace(" ", "")
+            .Replace(" ", "")
+            .Replace(" ", "")
+            .Replace("“", "")
+            .Replace("”", "")
+            .Replace(":", "")
+            .Replace("：", "")
+            .Replace("!", "")
+            .Replace("-", "")
+            .Replace(".", "")
+            .Replace("（", "")
+            .Replace("）", "")
+            .Replace("「", "")
+            .Replace("」", "")
+            .Replace("『", "")
+            .Replace("』", "")
+            .Replace("Recipe", "");
+    }
+}
