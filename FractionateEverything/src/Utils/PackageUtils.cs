e199bc4a1482e7f86dd62e29b71278ee8e6fc30b
乡下来的喵 <38677850+xxldm@users.noreply.github.com>
2025-12-30 15:53:04 +0800

    单人模式数据中心交互爆炸了！！！

--- FractionateEverything/src/Utils/PackageUtils.cs
+++ FractionateEverything/src/Utils/PackageUtils.cs
@@ -102,7 +102,7 @@ public static partial class Utils {
                 TechManager.CheckTechUnlockCondition(itemId);
             }
         }
-        if (manual) {
+        if (NebulaModAPI.IsMultiplayerActive && manual) {
             NebulaModAPI.MultiplayerSession.Network.SendPacket(new CenterItemChangePacket(itemId, count, inc));
         }
     }
@@ -899,7 +899,7 @@ public static partial class Utils {
                     centerItemInc[itemId] = 0;
                 }
             }
-            if (manual) {
+            if (NebulaModAPI.IsMultiplayerActive && manual) { 
                 NebulaModAPI.MultiplayerSession.Network.SendPacket(new CenterItemChangePacket(itemId, -count, -inc));
             }
             return count;
