c015e50809044e5191e7061b7faffd0bd7991dba
萌泪酱 <605738729@qq.com>
2026-02-02 19:37:09 +0800

    修复符文加成不显示+的问题

--- FractionateEverything/src/Utils/FormatUtils.cs
+++ FractionateEverything/src/Utils/FormatUtils.cs
@@ -24,6 +24,25 @@ public static partial class Utils {
     }
 
     /// <summary>
+    ///     将浮点数转化为百分数，至少保留一位有效数字。
+    /// </summary>
+    public static string FormatPWithSymbol(this float value) {
+        if (value < 0) {
+            return "-" + (-value).FormatPWithSymbol();
+        }
+        int i = 3;
+        var sb = new StringBuilder("+0.###");
+        while (i < 10) {
+            if (value * Math.Pow(10, i) >= 1.0) {
+                return value.ToString(sb.Append("%").ToString());
+            }
+            sb.Append("#");
+            i++;
+        }
+        return "+0%";
+    }
+
+    /// <summary>
     /// 格式化物品/配方名称，去除多余的空格、特殊字符等。
     /// </summary>
     public static string FormatName(string s) {
