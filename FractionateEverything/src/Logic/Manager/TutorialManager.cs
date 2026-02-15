3f58f818a2bc3025ea91e80fb72509152dc04963
萌泪酱 <605738729@qq.com>
2026-02-15 15:20:49 +0800

    修复指引缺失的bug

--- FractionateEverything/src/Logic/Manager/TutorialManager.cs
+++ FractionateEverything/src/Logic/Manager/TutorialManager.cs
@@ -1,5 +1,8 @@
-﻿using HarmonyLib;
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using HarmonyLib;
 using UnityEngine.UI;
+using xiaoye97;
 using static FE.Utils.Utils;
 
 namespace FE.Logic.Manager;
@@ -436,24 +439,21 @@ public static class TutorialManager {
         AddTutorial("物流交互站使用指南", "TOR_OnBuild", [IFE行星内物流交互站, IFE星际物流交互站]);
     }
 
-    private static int currTutorialID = 51;
+    private static int currTutorialID = 201;
 
     private static void AddTutorial(string name, string determinatorName, long[] determinatorParams) {
-        // TutorialProto proto = new() {
-        //     ID = currTutorialID,
-        //     SID = "",
-        //     Name = $"{name}标题",
-        //     name = $"{name}标题",
-        //     // PreText = $"{name}前字",
-        //     // PostText = $"{name}后字",
-        //     // Video = "",
-        //     DeterminatorName = determinatorName,
-        //     DeterminatorParams = determinatorParams,
-        //     LayoutFileName = $"tutorial-fe-{currTutorialID - 50}"
-        // };
-        // LDBTool.PreAddProto(proto);
-        // proto.Preload();
-        // currTutorialID++;
+        TutorialProto proto = new() {
+            ID = currTutorialID,
+            SID = "",
+            Name = $"{name}标题",
+            name = $"{name}标题",
+            LayoutFileName = $"tutorial-fe-{currTutorialID}",
+            DeterminatorName = determinatorName,
+            DeterminatorParams = determinatorParams,
+        };
+        LDBTool.PreAddProto(proto);
+        proto.Preload();
+        currTutorialID++;
     }
 
     /// <summary>
@@ -471,5 +471,82 @@ public static class TutorialManager {
             __instance.entryList.m_ScrollRect.enabled = false;
             __instance.entryList.m_ScrollRect.enabled = true;
         }
+    }
+
+    [HarmonyPatch(typeof(UITutorialWindow), nameof(UITutorialWindow.OnTutorialChange))]
+    [HarmonyTranspiler]
+    public static IEnumerable<CodeInstruction> UITutorialWindow_Transpiler(
+        IEnumerable<CodeInstruction> instructions, ILGenerator ilGenerator) {
+        var matcher = new CodeMatcher(instructions, ilGenerator);
+
+        /*
+            string layoutStr = UILayoutParserManager.GetLayoutStr(UITutorialWindow.textFolder, this.tutorialProto.LayoutFileName);
+
+            Ldarg_0
+            Ldfld tutorialProto
+            Call IsFELayout
+            Brfalse_S originalLogicLabel
+
+            Ldarg_0
+            Ldfld tutorialProto
+            Call GetLayoutStr
+            Br_S endLabel
+
+            IL_0027: ldsfld       string UITutorialWindow::textFolder // originalLogicLabel
+            IL_002c: ldarg.0      // this
+            IL_002d: ldfld        class TutorialProto UITutorialWindow::tutorialProto
+            IL_0032: ldfld        string TutorialProto::LayoutFileName
+            IL_0037: call         string UILayoutParserManager::GetLayoutStr(string, string)
+            IL_003c: stloc.0      // layoutStr // endLabel
+
+         */
+
+        matcher.MatchForward(false, new CodeMatch(OpCodes.Stloc_0));
+        matcher.CreateLabelAt(matcher.Pos, out var endLabel);
+
+        matcher.MatchBack(false,
+            new CodeMatch(OpCodes.Ldsfld,
+                AccessTools.Field(typeof(UITutorialWindow), nameof(UITutorialWindow.textFolder))));
+        matcher.CreateLabelAt(matcher.Pos, out var originalLogicLabel);
+
+        // 插入预加载和判断
+        matcher.InsertAndAdvance(
+            new CodeInstruction(OpCodes.Ldarg_0),
+            new CodeInstruction(OpCodes.Ldfld,
+                AccessTools.Field(typeof(UITutorialWindow), nameof(UITutorialWindow.tutorialProto))),
+            new CodeInstruction(OpCodes.Call,
+                AccessTools.Method(typeof(TutorialManager), nameof(IsFELayout))),
+            new CodeInstruction(OpCodes.Brfalse_S, originalLogicLabel)
+        );
+
+        matcher.InsertAndAdvance(
+            new CodeInstruction(OpCodes.Ldarg_0),
+            new CodeInstruction(OpCodes.Ldfld,
+                AccessTools.Field(typeof(UITutorialWindow), nameof(UITutorialWindow.tutorialProto))),
+            new CodeInstruction(OpCodes.Call,
+                AccessTools.Method(typeof(TutorialManager), nameof(GetLayoutStr))),
+            new CodeInstruction(OpCodes.Br_S, endLabel)
+        );
+
+        return matcher.InstructionEnumeration();
+    }
+
+    public static bool IsFELayout(TutorialProto proto) {
+        var layoutFileName = proto.LayoutFileName;
+        return !string.IsNullOrEmpty(layoutFileName) && layoutFileName.StartsWith("tutorial-fe-");
+    }
+    
+    const string preText =
+        "{$Text|fontsize=16;linespacing=1.1;textalignment=0,1;color=#FFFFFF52;material=UI/Materials/widget-text-alpha-5x-thick;margins=20,20,20,30}\n";
+    const string postText =
+        "{$Text|fontsize=14;linespacing=1.1;textalignment=0,1;color=#FFFFFF52;material=UI/Materials/widget-text-alpha-5x-thick;margins=20,20,20,20}\n";
+
+    public static string GetLayoutStr(TutorialProto proto) {
+        string protoName = proto.Name;
+        if (!protoName.EndsWith("标题")) {
+            return string.Empty;
+        }
+        var text = protoName.Replace("标题", "前字");
+        return $"{preText}{protoName.Translate()}{postText}{text.Translate()}";
     }
 }
