fc36ccd846b4bd05edfe2de311225d580fb5bd5f
萌泪酱 <605738729@qq.com>
2026-02-24 09:21:28 +0800

    修复下拉框显示错误的问题
    (cherry picked from commit acd62b786f0dec694ebd840571f547f9ce45ff97)

--- FractionateEverything/src/UI/Patches/UIComboBoxPatch.cs
+++ FractionateEverything/src/UI/Patches/UIComboBoxPatch.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Reflection.Emit;
-using FE.Compatibility;
 using HarmonyLib;
 using UnityEngine;
 
@@ -8,7 +7,6 @@ namespace FE.UI.Patches;
 
 public static class UIComboBoxPatch {
     /// <summary>
-    /// Bring ComboBox dropdown to top layer.
     /// 使ComboBox的下拉列表在最上层显示，不被其他UI元素覆盖。
     /// </summary>
     [HarmonyTranspiler]
@@ -19,7 +17,7 @@ public static class UIComboBoxPatch {
         //     return instructions;
         // }
         var matcher = new CodeMatcher(instructions, generator);
-        // 在 m_DropDownList.gameObject.SetActive(this.isDroppedDown) 之后插入 SetAsLastSibling
+        // 在 m_DropDownList.gameObject.SetActive(this.isDroppedDown) 之后插入代码来确保下拉框在最上层
         matcher.MatchForward(false,
             new CodeMatch(OpCodes.Ldarg_0),
             new CodeMatch(OpCodes.Ldfld, AccessTools.Field(typeof(UIComboBox), "m_DropDownList")),
@@ -33,17 +31,32 @@ public static class UIComboBoxPatch {
         );
         matcher.Advance(1);
         matcher.Insert(
+            // 调用辅助方法来设置下拉框的渲染层级
             new CodeInstruction(OpCodes.Ldarg_0),
             new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(UIComboBox), "m_DropDownList")),
-            new CodeInstruction(OpCodes.Callvirt,
-                AccessTools.PropertyGetter(typeof(Transform), nameof(Transform.parent))),
-            new CodeInstruction(OpCodes.Callvirt,
-                AccessTools.Method(typeof(Transform), nameof(Transform.SetAsLastSibling))),
             new CodeInstruction(OpCodes.Ldarg_0),
-            new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(UIComboBox), "m_DropDownList")),
-            new CodeInstruction(OpCodes.Callvirt,
-                AccessTools.Method(typeof(Transform), nameof(Transform.SetAsLastSibling)))
+            new CodeInstruction(OpCodes.Call,
+                AccessTools.PropertyGetter(typeof(UIComboBox), nameof(UIComboBox.isDroppedDown))),
+            new CodeInstruction(OpCodes.Call,
+                AccessTools.Method(typeof(UIComboBoxPatch), nameof(EnsureDropdownOnTop)))
         );
         return matcher.InstructionEnumeration();
+    }
+
+    /// <summary>
+    /// 确保下拉框显示在最上层
+    /// </summary>
+    private static void EnsureDropdownOnTop(Transform dropdownTransform, bool isDroppedDown) {
+        if (dropdownTransform == null) {
+            return;
+        }
+        if (isDroppedDown) {
+            // 在SetState中也调用SetAsLastSibling，确保每次Update都保持在最上层
+            // 因为其他UI元素可能在Update中改变层级顺序
+            if (dropdownTransform.parent != null) {
+                dropdownTransform.parent.SetAsLastSibling();
+            }
+            dropdownTransform.SetAsLastSibling();
+        }
     }
 }
