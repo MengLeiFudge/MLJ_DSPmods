662398500576503906fe3b4de015a1a09280c996
萌泪酱 <605738729@qq.com>
2026-05-16 01:23:13 +0800

    调整：收窄星环快速更新审计范围

--- AfterBuildEvent/src/DspCalcQuickUpdate/Mods/OrbitalRingQuickUpdate.cs
+++ AfterBuildEvent/src/DspCalcQuickUpdate/Mods/OrbitalRingQuickUpdate.cs
@@ -7,9 +7,14 @@ internal static class OrbitalRingQuickUpdate {
         return new() {
             SourceConfig = config,
             AuditPathPrefixes = [
-                "src/Protos/",
-                "src/Utils/",
-                "src/ProjectOrbitalRing.cs",
+                "data/items_mod.json",
+                "data/items_vanilla.json",
+                "data/prefabDescs.json",
+                "data/recipes.json",
+                "data/techs.json",
+                "src/Utils/JsonDataUtils.cs",
+                "src/Utils/JsonHelper.cs",
+                "src/Utils/ProtoID.cs",
             ],
             ReadSourceVersion = spec => QuickUpdateHelpers.ReadVersionConst(
                 spec.RequireFile("src", "ProjectOrbitalRing.cs")),
